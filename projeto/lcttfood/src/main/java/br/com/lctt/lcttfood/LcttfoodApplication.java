9c3ff8587d6a29b179eb2c0c326f19bc39c1a377
Lucas Ribeiro <lucasribeiro20@hotmail.com>
2022-01-11 13:24:59 -0300

    Criação do projeto.

---
+++ projeto/lcttfood/src/main/java/br/com/lctt/lcttfood/LcttfoodApplication.java
@@ -0,0 +1,13 @@
+package br.com.lctt.lcttfood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LcttfoodApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LcttfoodApplication.class, args);
+	}
+
+}
---
+++ projeto/lcttfood/src/test/java/br/com/lctt/lcttfood/LcttfoodApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.lctt.lcttfood;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LcttfoodApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
