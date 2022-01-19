8681810869bb94cad8bb8115db4b6df5cecbeab3
Lucas Ribeiro <lucasribeiro20@hotmail.com>
2022-01-19 11:14:00 -0300

    Aula 5.9. Usando queries JPQL customizadas com @Query

--- projeto/lcttfood/src/main/java/br/com/lctt/lcttfood/domain/repository/RestauranteRepository.java
+++ projeto/lcttfood/src/main/java/br/com/lctt/lcttfood/domain/repository/RestauranteRepository.java
@@ -7,10 +7,15 @@ import java.util.Optional;
 import br.com.lctt.lcttfood.domain.model.Cozinha;
 import br.com.lctt.lcttfood.domain.model.Restaurante;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface RestauranteRepository extends JpaRepository<Restaurante, Long> {
 
     List<Restaurante> queryByTaxaFreteBetween(BigDecimal taxaInicial, BigDecimal taxaFinal);
+
+    @Query("from Restaurante where nome like %:nome% and cozinha.id = :id")
+    List<Restaurante> consultarPorNome(String nome, @Param("id") Long cozinha);
 
     List<Restaurante> findByNomeContainingAndCozinhaId(String nome, Long cozinha);
 
