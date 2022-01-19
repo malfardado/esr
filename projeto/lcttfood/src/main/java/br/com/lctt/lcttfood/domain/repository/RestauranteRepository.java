9411fa799749f53eac644a77d1fb3a796599f556
Lucas Ribeiro <lucasribeiro20@hotmail.com>
2022-01-19 10:57:42 -0300

    Aula 5.8. Conhecendo os prefixos de query methods

--- projeto/lcttfood/src/main/java/br/com/lctt/lcttfood/domain/repository/RestauranteRepository.java
+++ projeto/lcttfood/src/main/java/br/com/lctt/lcttfood/domain/repository/RestauranteRepository.java
@@ -2,6 +2,7 @@ package br.com.lctt.lcttfood.domain.repository;
 
 import java.math.BigDecimal;
 import java.util.List;
+import java.util.Optional;
 
 import br.com.lctt.lcttfood.domain.model.Cozinha;
 import br.com.lctt.lcttfood.domain.model.Restaurante;
@@ -9,8 +10,14 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface RestauranteRepository extends JpaRepository<Restaurante, Long> {
 
-    List<Restaurante> findByTaxaFreteBetween(BigDecimal taxaInicial, BigDecimal taxaFinal);
+    List<Restaurante> queryByTaxaFreteBetween(BigDecimal taxaInicial, BigDecimal taxaFinal);
 
     List<Restaurante> findByNomeContainingAndCozinhaId(String nome, Long cozinha);
+
+    Optional<Restaurante> findFirstRestauranteByNomeContaining(String nome);
+
+    List<Restaurante> findTop2ByNomeContaining(String nome);
+
+    int countByCozinhaId(Long cozinhId);
 
 }
