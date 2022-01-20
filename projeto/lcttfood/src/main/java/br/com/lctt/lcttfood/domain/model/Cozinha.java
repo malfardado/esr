c7d3e652bbafbedbf78f2992ac4427d680236804
Lucas Ribeiro <lucasribeiro20@hotmail.com>
2022-01-20 08:50:03 -0300

    Aula 6.1. Mapeando relacionamento bidirecional com @OneToMany

--- projeto/lcttfood/src/main/java/br/com/lctt/lcttfood/domain/model/Cozinha.java
+++ projeto/lcttfood/src/main/java/br/com/lctt/lcttfood/domain/model/Cozinha.java
@@ -1,11 +1,8 @@
 package br.com.lctt.lcttfood.domain.model;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonRootName;
 
@@ -13,6 +10,9 @@ import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @JsonRootName("cozinha")
 @Data
@@ -25,8 +25,10 @@ public class Cozinha {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 
-	//@JsonIgnore
-	//@JsonProperty("titulo")
 	@Column(nullable = false)
 	private String nome;
+
+	@JsonIgnore
+	@OneToMany(mappedBy = "cozinha")
+	private List<Restaurante> restaurantes = new ArrayList<>();
 }
