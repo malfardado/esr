e0184e38a4d4b3c7681e244b3785242a90d89ff5
Lucas Ribeiro <lucasribeiro20@hotmail.com>
2022-01-26 18:59:02 -0300

    Aula 6.2. Mapeando relacionamento muitos-para-muitos com @ManyToMany

--- projeto/lcttfood/src/main/resources/import.sql
+++ projeto/lcttfood/src/main/resources/import.sql
@@ -1,14 +1,18 @@
 insert into cozinha(id, nome) values (1, 'Tailandesa');
 insert into cozinha(id, nome) values (2, 'Indiana');
-insert into restaurante(nome, taxa_frete, cozinha_id) values ('Tik Tok', 22.5, 1);
+insert into restaurante(nome, taxa_frete, cozinha_id) values ('Thai Gourmet', 10, 1);
 insert into restaurante(nome, taxa_frete, cozinha_id) values ('Thai Delivery', 9.5, 1);
-insert into restaurante(nome, taxa_frete, cozinha_id) values ('Tuk Tuk Comida Indiana', 5, 1);
+insert into restaurante(nome, taxa_frete, cozinha_id) values ('Tuk Tuk Comida Indiana', 15, 2);
 
-insert into forma_pagamento(descricao) values ('Dinheiro');
 insert into forma_pagamento(descricao) values ('Cartao de Crédito');
+insert into forma_pagamento(descricao) values ('Cartao de Débito');
+insert into forma_pagamento(descricao) values ('Dinheiro');
+
 
 insert into permissao(nome, descricao) values ('cadastrar_usuario','permite realizar cadastro de usuários');
 
 insert into estado(id, nome) values (1, 'Minas Gerais');
 
 insert into cidade(nome, estado_id) values ('Belo Horizonte', 1);
+
+insert into restaurante_forma_pagamento (restaurante_id, forma_pagamento_id) values (1, 1), (1, 2), (1, 3), (2, 3), (3, 2), (3, 3);
