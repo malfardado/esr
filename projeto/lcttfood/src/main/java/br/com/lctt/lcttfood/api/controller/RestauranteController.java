f867a6d7daa4904cf62aebf6bb93ccd3e5b7d630
Lucas Ribeiro <lucasribeiro20@hotmail.com>
2022-01-27 12:11:24 -0300

    Aula 6.5. Testando e analisando o impacto da incorporação de classe na REST API

--- projeto/lcttfood/src/main/java/br/com/lctt/lcttfood/api/controller/RestauranteController.java
+++ projeto/lcttfood/src/main/java/br/com/lctt/lcttfood/api/controller/RestauranteController.java
@@ -76,7 +76,8 @@ public class RestauranteController {
 			Restaurante restauranteAtual = restauranteRepository.findById(restauranteId).get();
 
 			if (restauranteAtual != null) {
-				BeanUtils.copyProperties(restaurante, restauranteAtual, "id", "formasPagamento");
+				BeanUtils.copyProperties(restaurante, restauranteAtual, "id", "formasPagamento"
+						, "endereco" );
 
 				restauranteAtual = cadastroRestaurante.salvar(restauranteAtual);
 				return ResponseEntity.ok(restauranteAtual);
--- projeto/lcttfood/src/main/resources/import.sql
+++ projeto/lcttfood/src/main/resources/import.sql
@@ -1,8 +1,19 @@
 insert into cozinha(id, nome) values (1, 'Tailandesa');
 insert into cozinha(id, nome) values (2, 'Indiana');
-insert into restaurante(nome, taxa_frete, cozinha_id) values ('Thai Gourmet', 10, 1);
-insert into restaurante(nome, taxa_frete, cozinha_id) values ('Thai Delivery', 9.5, 1);
-insert into restaurante(nome, taxa_frete, cozinha_id) values ('Tuk Tuk Comida Indiana', 15, 2);
+
+insert into estado (id, nome) values (1, 'Minas Gerais');
+insert into estado (id, nome) values (2, 'São Paulo');
+insert into estado (id, nome) values (3, 'Ceará');
+
+insert into cidade (id, nome, estado_id) values (1, 'Uberlândia', 1);
+insert into cidade (id, nome, estado_id) values (2, 'Belo Horizonte', 1);
+insert into cidade (id, nome, estado_id) values (3, 'São Paulo', 2);
+insert into cidade (id, nome, estado_id) values (4, 'Campinas', 2);
+insert into cidade (id, nome, estado_id) values (5, 'Fortaleza', 3);
+
+insert into restaurante(nome, taxa_frete, cozinha_id, endereco_cidade_id, endereco_bairro, endereco_cep, endereco_complemento, endereco_logradouro, endereco_numero) values ('Thai Gourmet', 10, 1, 1,'Chapadão', '35650-000', 'n/a', 'Rua Siderpita', '580');
+insert into restaurante(nome, taxa_frete, cozinha_id, endereco_cidade_id, endereco_bairro, endereco_cep, endereco_complemento, endereco_logradouro, endereco_numero) values ('Thai Delivery', 9.5, 1, 1,'Chapadão', '35650-000', 'n/a', 'Rua Siderpita', '580');
+insert into restaurante(nome, taxa_frete, cozinha_id, endereco_cidade_id, endereco_bairro, endereco_cep, endereco_complemento, endereco_logradouro, endereco_numero) values ('Tuk Tuk Comida Indiana', 15, 2, 1,'Chapadão', '35650-000', 'n/a', 'Rua Siderpita', '580');
 
 insert into forma_pagamento(descricao) values ('Cartao de Crédito');
 insert into forma_pagamento(descricao) values ('Cartao de Débito');
@@ -10,9 +21,5 @@ insert into forma_pagamento(descricao) values ('Dinheiro');
 
 
 insert into permissao(nome, descricao) values ('cadastrar_usuario','permite realizar cadastro de usuários');
-
-insert into estado(id, nome) values (1, 'Minas Gerais');
-
-insert into cidade(nome, estado_id) values ('Belo Horizonte', 1);
 
 insert into restaurante_forma_pagamento (restaurante_id, forma_pagamento_id) values (1, 1), (1, 2), (1, 3), (2, 3), (3, 2), (3, 3);
